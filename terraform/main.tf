efd4c87234a50b1819e5e0dbe1fb32945c2b8820
ganesh ghube <ganesh.ghube@gmail.com>
2023-01-08 20:43:09 +0000

    added network

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,22 @@
-resource "aws_instance" "example" {
- count = 15
- ami = "ami-408c7f28"
- instance_type = "t2.micro"
+
+
+  module "vpc" {
+  source = "./modules/vpc"
+
+  project              = var.project
+  environment          = var.environment
+  region               = var.region
+  availability_zones   = var.availability_zones
+  vpc_cidr             = var.vpc_cidr
+  public_subnets_cidr  = var.public_subnets_cidr
+  private_subnets_cidr = var.private_subnets_cidr
 }
+
+
+  module "iam" {
+  source = "./modules/iam"
+
+  user_names              = var.user_names
+  environment             = var.environment
+  
+  }
