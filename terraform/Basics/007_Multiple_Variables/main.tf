23257a470c7a53190dd85b1ff30dea83e2252a37
ganesh ghube <ganesh.ghube@gmail.com>
2023-01-10 17:53:40 +0000

    basic

--- terraform/Basics/007_Multiple_Variables/main.tf
+++ terraform/Basics/007_Multiple_Variables/main.tf
@@ -12,4 +12,11 @@ output prinboth {
 	value = "your name is ${var.username} and your age is ${var.age}"
 }
 
-#Run terraform plan and provide input
+#Run Belo and provide input 
+#terraform plan 
+
+#Run below with variables
+#terraform plan -var "<VARIABLE_1_NAME>=<VARIABLE_1_VALUE>" -var "<VARIABLE_2_NAME>=<VARIABLE_2_VALUE>"
+#terraform plan -var "username=Ganesh" -var "age=30"
+
+
