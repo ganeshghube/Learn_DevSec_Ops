3c81866f93b54cd43e2740295bd86e1029124421
ganesh ghube <ganesh.ghube@gmail.com>
2023-01-10 19:20:37 +0000

    basics

---
+++ terraform/Basics/008_Variable_types/main.tf
@@ -0,0 +1,16 @@
+output printname{
+	value = "Hello, ${var.username}"
+}
+
+
+output printage {
+	value = "your age is ${var.age}"
+}
+
+
+output prinboth {
+	value = "your name is ${var.username} and your age is ${var.age}"
+}
+
+
+
