c9775c06e0148a6fada197b1df00205144c5cef8
ganesh ghube <ganesh.ghube@gmail.com>
2023-01-11 19:09:03 +0000

    merge

--- terraform/Basics/021_create_ssh_keypair/createinstance.tf
+++ terraform/Basics/021_create_ssh_keypair/createinstance.tf
@@ -3,6 +3,21 @@ resource "aws_key_pair" "sshkeys" {
   public_key = file("${path.module}/sshkeys.txt")
 }
 
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0b0ea68c435eb488d"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.sshkeys.key_name
+  tags = {
+    Name = "first-tf-instance"
+  }
+}
+
 output "keyname" {
-  value = aws_key_pair.sshkeys.id
+  value = aws_key_pair.sshkeys.key_name
+}
+
+output "ip" {
+  value = aws_instance.web.public_ip
 }
