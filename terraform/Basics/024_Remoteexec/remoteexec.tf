9b3b377e5135771766b4db5aed375a62e93d6b20
ganesh ghube <ganesh.ghube@gmail.com>
2023-01-16 16:09:02 +0000

    bsic

---
+++ terraform/Basics/024_Remoteexec/remoteexec.tf
@@ -0,0 +1,26 @@
+# creating instance.
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.key-tf.key_name
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  tags = {
+    Name = "first-tf-instance"
+  }
+  user_data = file("${path.module}/script.sh")
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("${path.module}/id_rsa")
+    host        = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "ifconfig > /tmp/ifconfig.output",
+      "echo 'hello gaurav'>/tmp/test.txt"
+    ]
+  }
+  provisioner "remote-exec" {
+    script = "./testscript.sh"
+  }
+}
