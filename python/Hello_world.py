0a99da49c76207cfe7c9a84ca8d9ec6a322b55c4
Ganesh Ghube <ganesh.ghube@gmail.com>
2023-08-24 21:05:41 +0100

    update variables

---
+++ python/Hello_world.py
@@ -0,0 +1,18 @@
+#print statement
+print ("Hello_World!")
+
+#Seperate Line code
+print ("Hello_World!\nHello_World!\nHello_World!" )
+
+#Concatinate
+print ("Hello" +"_"+ "World!")
+print("\nplay")
+
+
+print ("Combine first name and last name\n")
+firstname = input("What is your first name?\n")
+lastname = input("What is your lastname?\n")
+
+print ("Your full name is:" + firstname +" " + lastname)
+
+
---
+++ python/Variable.py
@@ -0,0 +1,12 @@
+name = "Ganesh"
+print (name)
+name = "Pooja"
+print (name)
+
+firstname = input("Enter your firstname\n")
+print ("Your firstname is:" + firstname)
+lastname = input("Enter your lastname\n")
+print ("Your lastname is:" + lastname)
+print ("your full name is:" + firstname + " " + lastname)
+print ("Length of your full name is :")
+print (len(firstname + lastname))
