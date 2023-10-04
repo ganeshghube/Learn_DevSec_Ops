a74c57e1bb4f8dc9cb18ed5e9c99432b7af0564d
Ganesh Ghube <ganesh.ghube@gmail.com>
2023-10-04 20:33:15 +0100

    changes

---
+++ python/HelloWorld.py
@@ -0,0 +1,5 @@
+# Hello World Print String
+print ("Hello_World!")
+print ('Hello_World!')
+print ("Hello 'Ganesh'World!")
+print ('Hello "Ganesh" World!')
---
+++ python/escape.py
@@ -0,0 +1,8 @@
+print("hello\"world\"world")
+print("I'm Aaravi")
+print("line a\nline b\nline c") #To print on next line
+print("name\tAaravi") #To Print tab
+print("This is \ backs\\a\s\h")   
+#print("This is backslash\")   #Syntax error
+print("This is backslash\\")  
+print("This is backslash\\\\\\")  
