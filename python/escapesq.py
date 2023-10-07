c42b0453cde789a31e115107241facca3e77a8c2
Ganesh Ghube <ganesh.ghube@gmail.com>
2023-10-07 08:31:16 +0100

    test

--- python/escapesq.py
+++ python/escapesq.py
@@ -5,10 +5,20 @@ print("Line  A  \tLine B")
 print("Line  A  \\tLine B")
 print("\"\'")
 
-
+######################################################
 print ("This is \\\\ double back sash")
 print ("There are /\\/\\/\\/\\ mountains")
 print ("He is \t awesome")
 print('\\\" \\n \\t \\\'')
+print (r"Line A \nLine B")
+print("\U0001F600")
+print("\N{grinning face}")
+print("\U0001F40D")
+print("\U0001F46B")
+######################################################
+print("this  is \\\\double back sash")
+
+
+
 
 
