08d704efa077f001409018ec3802c2ac84f3bddd
Ganesh Ghube <ganesh.ghube@gmail.com>
2024-10-17 17:38:12 +0100

    Test

--- python/HelloWorld.py
+++
@@ -1,5 +0,0 @@
-# Hello World Print String
-print ("Hello_World!")
-print ('Hello_World!')
-print ("Hello 'Ganesh'World!")
-print ('Hello "Ganesh" World!')
--- python/two_input.py
+++
@@ -1,3 +0,0 @@
-name,age=input("enter your name & age 'separated by a comma'").split(",")
-print(name)
-print(age)
